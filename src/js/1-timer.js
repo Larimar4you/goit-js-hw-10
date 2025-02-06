2b4f5ab4e5093f337f5cce7e91a3b1e4a08f391b
Larimar4you <Zajanezlaja@gmail.com>
2025-02-06 21:29:11 +0100

    git push

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,13 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
 
+// npm i flatpickr --save     - Встановлення через NPM
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+// npm install izitoast --save
